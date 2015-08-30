934a975f6754003694b1dd3e2342d20cec1dfb18
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-08-30 11:06:08 +0200

    Trying --noplugin option

--- vimscript/run_.bat
+++ vimscript/run_.bat
@@ -3,7 +3,8 @@
 @rem @echo VIM_CMD_FILE: %VIM_CMD_FILE%
 
 @rem @gvim -i NONE -u NORC -U NONE  -c "redir > %VIM_CMD_FILE%.out" -nNesS %VIM_CMD_FILE%.vim
-@gvim -i NONE -u NORC -U NONE                                       -nNesS %VIM_CMD_FILE%.vim
+@rem 30.08.2015: trying --noplugin option
+@gvim -i NONE -u NORC -U NONE --noplugin                            -nNesS %VIM_CMD_FILE%.vim
 
 @type %VIM_CMD_FILE%.out
 @del %VIM_CMD_FILE%.out
